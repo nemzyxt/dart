0220b174c900121e3906bfca8761cde08bf7bbea
nemzyxt <94122064+nemzyxt@users.noreply.github.com>
2021-12-26 22:15:40 +0300

    Add files via upload

---
+++ if_else.dart
@@ -0,0 +1,20 @@
+//Author : Nemuel Wainaina
+//if ... else demo in Dart
+
+void main(){
+    int a = 10;
+    int b = 15;
+    
+    if(a > b){
+        print("a is greater");
+    }
+    else if(a < b){
+        print("b is greater");
+    }
+    else{
+        print("a and b are equal");
+    }
+
+    print("The value of a is : ${a}");
+    print("The value of b is : ${b}");
+}
