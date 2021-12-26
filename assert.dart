5579f355f7c295845112027c151fad18cb1615af
nemzyxt <94122064+nemzyxt@users.noreply.github.com>
2021-12-26 22:09:24 +0300

    Add files via upload

---
+++ assert.dart
@@ -0,0 +1,11 @@
+//Author : Nemuel Wainaina
+//the assert keyword in Dart
+//It checks whether the expression returns true or false
+//and execution only continues if the expression returns true
+//otherwise, it generates an error
+
+void main(){
+    int a = 5;
+    assert(a == 5); //continues without an error ...
+    print("There was no error");
+}
