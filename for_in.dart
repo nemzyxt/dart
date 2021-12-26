5a06767f1284b4c9265414abb9e60fbe9cbf3876
nemzyxt <94122064+nemzyxt@users.noreply.github.com>
2021-12-26 22:12:51 +0300

    Add files via upload

---
+++ for_in.dart
@@ -0,0 +1,11 @@
+//Author : Nemuel Wainaina
+//for ... in demo in Dart
+
+void main(){
+    //create a list of strings called names
+    var names = ["Nemuel", "Smith", "Harin", "Aleksi"];
+
+    for(var name in names){
+        print(name);
+    }
+}
