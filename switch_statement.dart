ea7bd7953e0a6b84197e8ef45cf5c21d32b63718
nemzyxt <94122064+nemzyxt@users.noreply.github.com>
2021-12-26 22:21:16 +0300

    Add files via upload

---
+++ switch_statement.dart
@@ -0,0 +1,33 @@
+//Author : Nemuel Wainaina
+//Switch statement demo in Dart
+
+void main(){
+    int dayNum = 5;
+
+    switch(dayNum){
+    case 1:
+        print("Monday");
+        break;
+    case 2:
+        print("Tuesday");
+        break;
+    case 3:
+        print("Wednesday");
+        break;
+    case 4:
+        print("Thursday");
+        break;
+    case 5:
+        print("Friday");
+        break;
+    case 6:
+        print("Saturday");
+        break;
+    case 7:
+        print("Sunday");
+        break;
+    default:
+        print("Invalid Number");
+    }
+
+}
